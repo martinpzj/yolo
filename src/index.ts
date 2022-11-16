19ac71fe11c669dfd524998af95a24a390d6868a
martinpzj <martin03pz@gmail.com>
2022-11-15 21:50:59 -0800

    initial api requests

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,39 @@
-console.log('yolo')
+import axios, { AxiosInstance } from "axios";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const BASE_URL = "https://api.yelp.com/v3/businesses/search";
+const RADIUS = 40000;
+const LIMIT = 50;
+
+function createQueryParams(offset: number) {
+  return {
+    location: "CA",
+    latitude: process.env.LATITUDE,
+    longitude: process.env.LONGITUDE,
+    radius: RADIUS,
+    limit: LIMIT,
+    offset: offset,
+  };
+}
+
+async function main() {
+  const apiKey = process.env["API_KEY"];
+
+  try {
+    const response = await axios.get(BASE_URL, {
+      headers: {
+        Authorization: `Bearer ${apiKey}`,
+      },
+      params: createQueryParams(50),
+    });
+
+    console.log(response.data);
+  } catch (e) {
+    throw new Error(`Something went wrong: ${e}`);
+  }
+}
+
+console.log("yolo");
+main();
